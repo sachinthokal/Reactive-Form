78aa163abeba3a4146ac856839be48caa3ae988f
sachinthokal <sachinthokal1799@gmail.com>
2022-12-24 16:23:51 +0530

    Form Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Reactive-Form';
+
+  Uname: string = 'Sachin';
+  Pass: string = '12345';
+  Mob: number = 88171399;
+
+  userForm = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+    mobile: new FormControl('', [Validators.required]),
+    checkme: new FormControl('', [Validators.required]),
+  });
+
+  onFormSubmit() {
+    console.log('Name ' + this.userForm.value['username']);
+    console.log(this.userForm.value);
+    console.log('Validation Status: ' + this.userForm.status);
+  }
 }
